2ba67e2f3cbbd1b88993e6435b9335bfa8856ce1
AlejandroSuero <asuerome@gmail.com>
2023-09-19 16:25:12 +0200

    Fixed not showing port on production URL

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ export const createApp = ({ movieModel }) => {
 
   const PORT = process.env.PORT ?? 3000
   const URL = process.env.NODE_ENV === "production"
-    ? `https://nodejs-api-fss8-dev.fl0.io:${PORT}`
+    ? "https://nodejs-api-fss8-dev.fl0.io"
     : `http://localhost:${PORT}`
 
   app.listen(PORT, () => {
